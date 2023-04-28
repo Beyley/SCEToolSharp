2240341bf52cafee10fb431464198832749a9e14
jvyden <jvyden@jvyden.xyz>
2023-04-27 22:40:00 -0400

    Update libscetool builds for Linux and Mac

--- SCEToolSharp.Test/Program.cs
+++ SCEToolSharp.Test/Program.cs
@@ -1,3 +1,4 @@
 ﻿using SCEToolSharp;
 
-LibSceToolSharp.PrintInfos("/home/jvyden/.config/rpcs3/dev_hdd0/game/BCES00850/USRDIR/EBOOT.BIN");
+LibSceToolSharp.PrintInfos("/home/jvyden/.config/rpcs3/dev_hdd0/game/BCUS98208/USRDIR/EBOOT.BIN");
+LibSceToolSharp.Decrypt("/home/jvyden/.config/rpcs3/dev_hdd0/game/BCUS98208/USRDIR/EBOOT.BIN", "/tmp/EBOOT.ELF");
