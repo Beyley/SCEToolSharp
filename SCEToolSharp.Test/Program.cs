cf7708957224695184a5f217de1bd155734bec66
jvyden <jvyden@jvyden.xyz>
2023-04-27 21:55:22 -0400

    Add test project, call libscetool_init, update natives

---
+++ SCEToolSharp.Test/Program.cs
@@ -0,0 +1,3 @@
+﻿using SCEToolSharp;
+
+LibSceToolSharp.PrintInfos("/home/jvyden/.config/rpcs3/dev_hdd0/game/BCES00850/USRDIR/EBOOT.BIN");
--- SCEToolSharp/LibSCEToolSharp.cs
+++ SCEToolSharp/LibSCEToolSharp.cs
@@ -6,7 +6,11 @@ namespace SCEToolSharp;
 
 [SuppressMessage("ReSharper", "UnusedType.Global")]
 [SuppressMessage("ReSharper", "UnusedMember.Global")]
-public static unsafe class LibSceToolSharp {
+public static unsafe class LibSceToolSharp
+{
+	[DllImport("scetool")]
+	private static extern int libscetool_init();
+	
 	[DllImport("scetool")]
 	private static extern void frontend_print_infos(byte* file);
 
@@ -16,19 +20,27 @@ public static unsafe class LibSceToolSharp {
 	[DllImport("scetool")]
 	private static extern void frontend_encrypt(byte* fileIn, byte* fileOut);
 
-	public static void Decrypt(string input, string output) {
+	static LibSceToolSharp()
+	{
+		libscetool_init();
+	}
+
+	public static void Decrypt(string input, string output)
+	{
 		fixed (byte* inputPtr = Encoding.ASCII.GetBytes(input + "\0"))
 			fixed (byte* outputPtr = Encoding.ASCII.GetBytes(output + "\0"))
 				frontend_decrypt(inputPtr, outputPtr);
 	}
 	
-	public static void Encrypt(string input, string output) {
+	public static void Encrypt(string input, string output)
+	{
 		fixed (byte* inputPtr = Encoding.ASCII.GetBytes(input + "\0"))
 			fixed (byte* outputPtr = Encoding.ASCII.GetBytes(output + "\0"))
 				frontend_encrypt(inputPtr, outputPtr);
 	}
 	
-	public static void PrintInfos(string file) {
+	public static void PrintInfos(string file)
+	{
 		fixed (byte* filePtr = Encoding.ASCII.GetBytes(file + "\0"))
 			frontend_print_infos(filePtr);
 	}
