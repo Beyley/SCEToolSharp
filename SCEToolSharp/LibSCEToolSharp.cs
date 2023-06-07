076f9bee67e87b482a9f73553df7df2e19709a5c
jvyden <jvyden@jvyden.xyz>
2023-06-07 17:55:44 -0400

    Use UTF8 encoding when marshalling

--- SCEToolSharp/LibSCEToolSharp.cs
+++ SCEToolSharp/LibSCEToolSharp.cs
@@ -30,21 +30,21 @@ public static unsafe partial class LibSceToolSharp
 
 	public static void Decrypt(string input, string output)
 	{
-		fixed (byte* inputPtr = Encoding.ASCII.GetBytes(input + "\0"))
-			fixed (byte* outputPtr = Encoding.ASCII.GetBytes(output + "\0"))
+		fixed (byte* inputPtr = Encoding.UTF8.GetBytes(input + "\0"))
+			fixed (byte* outputPtr = Encoding.UTF8.GetBytes(output + "\0"))
 				frontend_decrypt(inputPtr, outputPtr);
 	}
 	
 	public static void Encrypt(string input, string output)
 	{
-		fixed (byte* inputPtr = Encoding.ASCII.GetBytes(input + "\0"))
-			fixed (byte* outputPtr = Encoding.ASCII.GetBytes(output + "\0"))
+		fixed (byte* inputPtr = Encoding.UTF8.GetBytes(input + "\0"))
+			fixed (byte* outputPtr = Encoding.UTF8.GetBytes(output + "\0"))
 				frontend_encrypt(inputPtr, outputPtr);
 	}
 	
 	public static void PrintInfos(string file)
 	{
-		fixed (byte* filePtr = Encoding.ASCII.GetBytes(file + "\0"))
+		fixed (byte* filePtr = Encoding.UTF8.GetBytes(file + "\0"))
 			frontend_print_infos(filePtr);
 	}
 
@@ -52,7 +52,7 @@ public static unsafe partial class LibSceToolSharp
 	{
 		if (!Directory.Exists(dirPath)) throw new DirectoryNotFoundException(dirPath);
 		
-		fixed (byte* dirPtr = Encoding.ASCII.GetBytes(dirPath + '\0'))
+		fixed (byte* dirPtr = Encoding.UTF8.GetBytes(dirPath + '\0'))
 			rap_set_directory(dirPtr);
 	}
 }
